756ce037dbb2365c44b2241661e17609b83b5439
David McGrath <github.com@davidmcgrath.com>
2019-01-03 14:22:44 -0500

    configure eslint
    with 'eslint:recommended' settings, using babel to parse, and also having some particular rules that I like for development.
    
    also setting webpack to automatically fix simple errors

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,15 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
       },
     ],
