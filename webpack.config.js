fdd6e9a18720549b514b892cd3f19731962d2626
David McGrath <github.com@davidmcgrath.com>
2019-01-03 13:54:54 -0500

    configure html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path');
 
 module.exports = {
@@ -10,4 +11,10 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'your life in dots',
+    }),
+  ],
 };
